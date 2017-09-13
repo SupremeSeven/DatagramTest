dddc36a8a7d18729a5d95fa1a520242fb27424fc
Daniel Muldrew <dmuldrew@yahoo.com>
2017-09-13 12:06:58 -0700

    Reflect generated SQL statements

--- DatagramTest/Program.cs
+++ DatagramTest/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Xml.Serialization;
 using System.Xml.Schema;
+using System.Reflection;
 
 namespace DatagramTest
 {
@@ -15,8 +16,8 @@ namespace DatagramTest
 
         static void Main()
         {
-            using (StreamReader datagramReader = new StreamReader(@"..\..\Datagramv1.1.xml"))
-//           using (StreamReader datagramReader = new StreamReader(@"..\..\TestDatagram.xml"))
+//            using (StreamReader datagramReader = new StreamReader(@"..\..\Datagramv1.1.xml"))
+            using (StreamReader datagramReader = new StreamReader(@"..\..\TestDatagram.xml"))
             {
                 XmlSerializer serializer = new XmlSerializer(typeof(Datagram));
                 data = (Datagram)serializer.Deserialize(datagramReader);
@@ -24,16 +25,49 @@ namespace DatagramTest
 
             Console.WriteLine("Woohoo!");
 
-            PrintData();
+//            PrintProperties(data);
+
+            GenericEntity<DatagramTransactionOrderAssembly> SQLtest = new GenericEntity<DatagramTransactionOrderAssembly>(data.Transaction.Order.Assembly[0]);
+
+            string query = SQLtest.Insert();
+            Console.WriteLine(query);
         }
 
         static void PrintData()
         {
             Console.WriteLine($"{data.Id}");
             Console.WriteLine($"{data.Timestamp}");
-            Console.WriteLine($"{data.Transaction.Customer}");
+            Console.WriteLine($"{data.Transaction.Customer.Name}");
+            Console.WriteLine($"{data.Transaction.Customer.Address[0].AddressLine1}");
+            Console.WriteLine($"{data.Transaction.Customer.Name}");
             Console.WriteLine($"{data.Transaction.Order}");
         }
-  
+
+        static public void PrintProperties(object obj)
+        {
+            PrintProperties(obj, 0);
+        }
+
+        static public void PrintProperties(object obj, int indent)
+        {
+            if (obj == null) return;
+            string indentString = new string(' ', indent);
+            Type objType = obj.GetType();
+            PropertyInfo[] properties = objType.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                object propValue = property.GetValue(obj, null);
+                if (property.PropertyType.Assembly == objType.Assembly && !property.PropertyType.IsEnum)
+                {
+                    Console.WriteLine("{0}{1}:", indentString, property.Name);
+                    PrintProperties(propValue, indent + 2);
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}: {2}", indentString, property.Name, propValue);
+                }
+            }
+        }
+
     }
 }
---
+++ DatagramTest/ReflectionSQL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DatagramTest
+{
+    public class GenericEntity<T>
+    {
+        private T obj;
+
+        public GenericEntity(T obj)
+        {
+            this.obj = obj;
+        }
+        public string Insert()
+        {
+            StringBuilder sb = new StringBuilder();
+            int ctr = 0;
+
+            Type type = typeof(T);
+            StringBuilder sbQry = new StringBuilder();
+
+
+            PropertyInfo[] propInfo = type.GetProperties();
+            foreach (PropertyInfo pi in propInfo)
+            {
+                if (sbQry.ToString() == string.Empty)
+                    sbQry.AppendFormat("INSERT INTO {0} ({1}",
+                       type.Name.Replace("Entity", string.Empty), pi.Name);
+                else
+                {
+                    sbQry.AppendFormat(", {0}", pi.Name);
+                    sb.Append(",");
+                }
+                sb.Append("{" + ctr++ + "}");
+                Console.WriteLine(pi.GetValue(obj));
+            }
+
+            if (sbQry.ToString() != string.Empty)
+                sbQry.AppendFormat(") VALUES({0})", sb.ToString());
+
+            return sbQry.ToString();
+        }
+    }
+}
+
